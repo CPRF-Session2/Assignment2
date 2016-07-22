79b0fe024466c7b35b7530c2717c81411eed540f
uityh.uityh <uityh.mtk2134@columbia.edu>
2016-07-21 20:29:37 -0400

    Matthew Kellerman

---
+++ evenodd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int y = 0;
+	int x;
+	printf("Please enter a number: \n");
+	scanf("%d", &x);
+
+	if (x%2 == y) {
+		printf("Your number is even.\n");
+	}
+	else {
+		printf("Your number is odd.\n");
+	}
+}
